7712d6d51724b192fc83365d1cd4af937fa4a2c3
Metin-Z <102662790+Metin-Z@users.noreply.github.com>
2023-09-23 17:43:45 +0300

    bounce

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,6 +10,7 @@ public class EnemyController : MonoBehaviour
     public NavMeshAgent _navMesh;
     public Vector3 Target;
     public bool final;
+    public Rigidbody rbSkeleton;
     PlayerController _playerController => PlayerController.instance;
     public void Catch()
     {
@@ -22,13 +23,20 @@ public class EnemyController : MonoBehaviour
         _navMesh.enabled = false;
         _anim.enabled = false;
         StartCoroutine(Run());
+        foreach (Rigidbody item in rbSkeleton.GetComponentsInChildren<Rigidbody>())
+        {
+            item.AddForce(Vector3.forward * GameManager.instance.score*5*PlayerController.instance.playerSpeed);
+            item.AddForce(Vector3.up * (GameManager.instance.score*5*PlayerController.instance.playerSpeed/4) /2);
+        }
+        //rb.constraints = RigidbodyConstraints.None;
     }
     public bool catchPlayer;
     public IEnumerator Run()
     {
         yield return new WaitForSeconds(1.75f);
-        if (!final)
+        if (!GameManager.instance.bonusLevel)
         {
+            //rb.constraints = RigidbodyConstraints.FreezePositionY;
             _anim.SetBool("Run", true);
             _anim.enabled = true;
             _navMesh.enabled = true;
@@ -54,6 +62,14 @@ public class EnemyController : MonoBehaviour
             {
                 _navMesh.SetDestination(Target);
             }
+        }
+    }
+
+    private void Awake()
+    {
+        foreach (CharacterJoint item in transform.GetComponentsInChildren<CharacterJoint>())
+        {
+            item.enableProjection = true;
         }
     }
 }
